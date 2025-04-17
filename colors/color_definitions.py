2877972f36bee9e47f11cdb4e36a63916d7253ae
Hanny <hannylicious@gmail.com>
2025-04-17 14:48:30 -0500

    Summary:Format fields.py; Eliminate enum inheritence (#5)
    Description:

--- colors/color_definitions.py
+++ colors/color_definitions.py
@@ -1,66 +1,87 @@
-
 from django.db.models import TextChoices
 
 
-
 class ColorChoices(TextChoices):
-    BLUE = 'blue', 'Blue'
-    GREEN = 'green', 'Green'
-    YELLOW = 'yellow', 'Yellow'
-    RED = 'red', 'Red'
-    PURPLE = 'purple', 'Purple'
-    INDIGO = 'indigo', 'Indigo'
-    PINK = 'pink', 'Pink'
-    ORANGE = 'orange', 'Orange'
-    TEAL = 'teal', 'Teal'
-    CYAN = 'cyan', 'Cyan'
-    GRAY = 'gray', 'Gray'
+    BLUE = "blue", "Blue"
+    GREEN = "green", "Green"
+    YELLOW = "yellow", "Yellow"
+    RED = "red", "Red"
+    PURPLE = "purple", "Purple"
+    INDIGO = "indigo", "Indigo"
+    PINK = "pink", "Pink"
+    ORANGE = "orange", "Orange"
+    TEAL = "teal", "Teal"
+    CYAN = "cyan", "Cyan"
+    GRAY = "gray", "Gray"
 
     @property
     def bg_css_class(self):
-        return ''
+        return ""
 
     @property
     def text_css_class(self):
-        return ''
+        return ""
 
     @classmethod
     def choices_list(cls):
-        options = [(member.value, member.label, member.bg_css_class, member.text_css_class) for member in cls]
+        options = [
+            (member.value, member.label, member.bg_css_class, member.text_css_class)
+            for member in cls
+        ]
         return options
-    
-class BootstrapColorChoices(ColorChoices):
+
+
+class BootstrapColorChoices(TextChoices):
+    BLUE = "blue", "Blue"
+    GREEN = "green", "Green"
+    YELLOW = "yellow", "Yellow"
+    RED = "red", "Red"
+    PURPLE = "purple", "Purple"
+    INDIGO = "indigo", "Indigo"
+    PINK = "pink", "Pink"
+    ORANGE = "orange", "Orange"
+    TEAL = "teal", "Teal"
+    CYAN = "cyan", "Cyan"
+    GRAY = "gray", "Gray"
+
     @property
     def bg_css_class(self):
         bg_classes = {
-            'blue': 'bg-primary-200',
-            'green': 'bg-success-200',
-            'yellow': 'bg-warning-200',
-            'red': 'bg-danger-200',
-            'purple': 'bg-purple-200',
-            'indigo': 'bg-indigo-200',
-            'pink': 'bg-pink-200',
-            'orange': 'bg-orange-200',
-            'teal': 'bg-teal-200',
-            'cyan': 'bg-cyan-200',
-            'gray': 'bg-gray-200',
+            "blue": "bg-primary-200",
+            "green": "bg-success-200",
+            "yellow": "bg-warning-200",
+            "red": "bg-danger-200",
+            "purple": "bg-purple-200",
+            "indigo": "bg-indigo-200",
+            "pink": "bg-pink-200",
+            "orange": "bg-orange-200",
+            "teal": "bg-teal-200",
+            "cyan": "bg-cyan-200",
+            "gray": "bg-gray-200",
         }
-        return bg_classes.get(self.value, '')
+        return bg_classes.get(self.value, "")
 
     @property
     def text_css_class(self):
         text_classes = {
-            'blue': 'text-primary',
-            'green': 'text-success',
-            'yellow': 'text-warning',
-            'red': 'text-danger',
-            'purple': 'text-purple',
-            'indigo': 'text-indigo',
-            'pink': 'text-pink',
-            'orange': 'text-orange',
-            'teal': 'text-teal',
-            'cyan': 'text-cyan',
-            'gray': 'text-gray',
+            "blue": "text-primary",
+            "green": "text-success",
+            "yellow": "text-warning",
+            "red": "text-danger",
+            "purple": "text-purple",
+            "indigo": "text-indigo",
+            "pink": "text-pink",
+            "orange": "text-orange",
+            "teal": "text-teal",
+            "cyan": "text-cyan",
+            "gray": "text-gray",
         }
-        return text_classes.get(self.value, '')
+        return text_classes.get(self.value, "")
 
+    @classmethod
+    def choices_list(cls):
+        options = [
+            (member.value, member.label, member.bg_css_class, member.text_css_class)
+            for member in cls
+        ]
+        return options
--- colors/fields.py
+++ colors/fields.py
@@ -7,41 +7,62 @@ from django.db.models.functions import Lower
 from .widgets import ColorChoiceWidget
 from .color_definitions import ColorChoices, BootstrapColorChoices
 
+
 class ColorModelField(CharField):
     choice_model: Model
     choice_queryset: QuerySet
     default_options: ColorChoices
 
-    def __init__(self, model=None, queryset=None, default_options = BootstrapColorChoices, *args, **kwargs):
+    def __init__(
+        self,
+        model=None,
+        queryset=None,
+        default_options=BootstrapColorChoices,
+        *args,
+        **kwargs,
+    ):
         self.choice_model = model
         self.choice_queryset = queryset
         self.default_options = default_options
-        kwargs.setdefault('max_length', 32)
+        kwargs.setdefault("max_length", 32)
         super().__init__(*args, **kwargs)
 
     def deconstruct(self):
         # modified deconstruct method to properly add args (model or queryset) in migration field definition.
         name, path, args, kwargs = super().deconstruct()
         if self.choice_model:
-            kwargs['model'] = self.choice_model
+            kwargs["model"] = self.choice_model
         if self.choice_queryset:
-            kwargs['queryset'] = self.choice_queryset
+            kwargs["queryset"] = self.choice_queryset
 
-        kwargs['default_options'] = self.default_options
-        
+        kwargs["default_options"] = self.default_options
+
         return name, path, args, kwargs
 
     def formfield(self, **kwargs):
-        """ Creates a forms.ChoiceField with a custom widget and choices. """
+        """Creates a forms.ChoiceField with a custom widget and choices."""
 
-        choices = self.default_options.choices_list() #default colors
+        choices = self.default_options.choices_list()  # default colors
 
         if self.choice_queryset is not None:
-            choices.extend(list(self.choice_queryset.values_list(Lower('name'), 'name', 'bg_css_class', 'text_css_class')))
+            choices.extend(
+                list(
+                    self.choice_queryset.values_list(
+                        Lower("name"), "name", "bg_css_class", "text_css_class"
+                    )
+                )
+            )
 
         elif self.choice_model is not None:
-            choices.extend(list(self.choice_model.objects.all().values_list(Lower('name'), 'name', 'bg_css_class', 'text_css_class')))
+            choices.extend(
+                list(
+                    self.choice_model.objects.all().values_list(
+                        Lower("name"), "name", "bg_css_class", "text_css_class"
+                    )
+                )
+            )
 
         widget = ColorChoiceWidget(choices=choices)
 
         return ChoiceField(choices=choices, widget=widget, **kwargs)
+
